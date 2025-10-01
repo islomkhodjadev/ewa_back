ebfc760f8b8141726fed42fb7636ce3f160d0e47
islomkhodja <islomkhodjadev@gmail.com>
2025-10-01 19:33:03 +0500

    addded unfold and verbose names correctly

--- telegram/admin.py
+++ telegram/admin.py
@@ -276,13 +276,26 @@ class BadTestSessionAdmin(ModelAdmin):
 
 
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from unfold.admin import ModelAdmin
 
+
 admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 @admin.register(User)
-class UserAdmin(ModelAdmin, UserAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     pass
